cdcb7a4deba9c23b21a3dd8f55ddf397b99c71a0
Bohdan Vakh <Bohdanvah@gmail.com>
2024-01-10 22:32:52 +0200

    feat/provide ActionCable

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # action cable server
+  mount ActionCable.server => "/cable"
   resources :messages
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
