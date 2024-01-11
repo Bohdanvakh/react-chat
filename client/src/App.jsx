472320e6b635b58f8cf0f9d3f30cf621fc5b1dc9
Bohdan Vakh <Bohdanvah@gmail.com>
2024-01-11 21:52:03 +0200

    feat/add logic that returns messages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,6 +8,7 @@ const ws = new WebSocket("ws://localhost:3000/cable");
 function App() {
   const [messages, setMessages] = useState([]);
   const [guid, setGuid] = useState("");
+  const messagesContainer = document.getElementById("messages");
 
   ws.onopen = () => {
     console.log("Connected to websocket server");
@@ -24,11 +25,34 @@ function App() {
     )
   };
 
+  useEffect(() => {
+    fetchMessages();
+  }, []);
+
+  const fetchMessages = async () => {
+    const response = await fetch("http://localhost:3000/messages");
+    const data = await response.json();
+    setMessagesAndScrollDown(data);
+  };
+
+  const setMessagesAndScrollDown = (data) => {
+    setMessages(data);
+    if (!messagesContainer) return;
+    messagesContainer.scrollTop = messagesContainer.scrollHeight;
+  };
+
   return (
     <div className="App">
       <div className="messageHeader">
         <h1>Messages</h1>
         <p>Guid: {guid}</p>
+      </div>
+      <div className="messages" id="messages">
+        {messages.map((message) => (
+          <div className="message" key={message.id}>
+            <p>{message.body}</p>
+          </div>
+        ))}
       </div>
     </div>
   )
