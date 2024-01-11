dac2160b3c775af7746bc772e75671d909395bed
Bohdan Vakh <Bohdanvah@gmail.com>
2024-01-11 22:20:11 +0200

    feat/provide messages sending logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,9 +25,37 @@ function App() {
     )
   };
 
+  ws.onmessage = (e) => {
+    const data = JSON.parse(e.data);
+    if (data.type === "ping") return;
+    if (data.type === "welcome") return;
+    if (data.type === "confirm_subscription") return;
+
+    const message = data.message;
+    setMessagesAndScrollDown([...messages, message]);
+  };
+
   useEffect(() => {
     fetchMessages();
   }, []);
+
+  useEffect(() => {
+    resetScroll();
+  }, [messages]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const body = e.target.message.value;
+    e.target.message.value = "";
+
+    await fetch("http://localhost:3000/messages", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ body }),
+    });
+  };
 
   const fetchMessages = async () => {
     const response = await fetch("http://localhost:3000/messages");
@@ -37,6 +65,10 @@ function App() {
 
   const setMessagesAndScrollDown = (data) => {
     setMessages(data);
+    resetScroll();
+  };
+
+  const resetScroll = () => {
     if (!messagesContainer) return;
     messagesContainer.scrollTop = messagesContainer.scrollHeight;
   };
@@ -53,6 +85,14 @@ function App() {
             <p>{message.body}</p>
           </div>
         ))}
+      </div>
+      <div className="messageForm">
+        <form onSubmit={handleSubmit}>
+          <input className="messageInput" type="text" name="message" />
+          <button className="messageButton" type="submit">
+            Send
+          </button>
+        </form>
       </div>
     </div>
   )
